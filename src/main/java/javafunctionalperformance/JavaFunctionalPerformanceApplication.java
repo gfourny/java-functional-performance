ab3b776e4314312d620181567687b0e8f945aba5
Guillaume Fourny <fourny.g@free.fr>
2023-10-14 13:12:10 +0200

    init project

---
+++ src/main/java/javafunctionalperformance/JavaFunctionalPerformanceApplication.java
@@ -0,0 +1,13 @@
+package javafunctionalperformance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFunctionalPerformanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFunctionalPerformanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/javafunctionalperformance/JavaFunctionalPerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package javafunctionalperformance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFunctionalPerformanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
